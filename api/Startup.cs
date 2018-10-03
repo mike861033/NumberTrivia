f431dbdf46411d390c0dabd789401b093d4258ff
mmolek <mmolek@SPARKL285>
2018-10-03 14:33:19 +0100

    working

--- api/Startup.cs
+++ api/Startup.cs
@@ -40,7 +40,7 @@ namespace api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
